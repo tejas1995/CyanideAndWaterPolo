60317b2e15ebf2cd8f2a6aed5adc761b66735afc
Tejas Srinivasan <tejas.srinivasan95@gmail.com>
2015-09-18 04:32:18 +0530

    Added function declarations for AI system

---
+++ ai.h
@@ -0,0 +1,14 @@
+#ifndef AI_H
+#define AI_H
+
+#include "structures.h"
+#include "enums.h"
+
+float distance(player* Player);
+float distance(player Player, ball Ball);
+float distance(player Player, goal Goal);
+float distance(ball Ball, goal Goal);
+void initCompKeyStates();
+int* getCompKeyStates(player*, goal*, ball);
+
+#endif
