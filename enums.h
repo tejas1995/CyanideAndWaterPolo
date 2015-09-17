8ba6f57e4671f71c9cdfc380fbd45e09b842bba3
Tejas Srinivasan <tejas.srinivasan95@gmail.com>
2015-09-17 16:20:29 +0530

    Moved all enums to enums.h; added mthods to load media and close textures for all objects, and method to poll for user input

---
+++ enums.h
@@ -0,0 +1,22 @@
+#ifndef ENUMS_H
+#define ENUMS_H
+
+enum PlayerCodes
+{
+    USER,
+    COMPUTER
+};
+
+enum KeyCodes
+{
+    KEY_W,
+    KEY_A,
+    KEY_S,
+    KEY_D,
+    KEY_SHIFT,
+    KEY_K,
+    KEY_L,
+    KEY_TOTAL
+};
+
+#endif
