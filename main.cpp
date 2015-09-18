14855d86d9c2e95779396b7e3fe6ce2d80d672f9
Manan Doshi <manan.doshi@iitb.ac.in>
2015-09-18 20:30:47 +0530

    stuff

--- main.cpp
+++ main.cpp
@@ -66,14 +66,14 @@ void initialize()
     Goal[ USER ].setY(200);
     Goal[ USER ].defineTopNet( Goal[ USER ].getX(), Goal[ USER ].getY(), GOAL_WIDTH, GOAL_THICK );
     Goal[ USER ].defineBackNet( SCREEN_WIDTH - GOAL_THICK, Goal[ USER ].getY(), GOAL_THICK, GOAL_HEIGHT );
-    Goal[ USER ].defineBlankSpace( Goal[ USER ].getX(), Goal[ USER ].getY() + GOAL_THICK, GOAL_WIDTH - GOAL_THICK, GOAL_HEIGHT - GOAL_THICK);
+    Goal[ USER ].defineBlankSpace( Goal[ USER ].getX(), Goal[ USER ].getY() + GOAL_THICK, GOAL_WIDTH - GOAL_THICK, GOAL_HEIGHT - GOAL_THICK + 10);
 
     //Initialize the computer goal
     Goal[ COMPUTER ].setX(0);
     Goal[ COMPUTER ].setY(200);
     Goal[ COMPUTER ].defineTopNet( Goal[ COMPUTER ].getX(), Goal[ COMPUTER ].getY(), GOAL_WIDTH, GOAL_THICK );
     Goal[ COMPUTER ].defineBackNet( Goal[COMPUTER].getX(), Goal[ COMPUTER ].getY(), GOAL_THICK, GOAL_HEIGHT );
-    Goal[ COMPUTER ].defineBlankSpace( Goal[ COMPUTER ].getX() + GOAL_THICK, Goal[ COMPUTER ].getY() + GOAL_THICK, GOAL_WIDTH - GOAL_THICK, GOAL_HEIGHT - GOAL_THICK);
+    Goal[ COMPUTER ].defineBlankSpace( Goal[ COMPUTER ].getX() + GOAL_THICK, Goal[ COMPUTER ].getY() + GOAL_THICK, GOAL_WIDTH - GOAL_THICK, GOAL_HEIGHT - GOAL_THICK + 10);
 
     //Initializethe water object
     Water.setDepth(WATER_DEPTH);
